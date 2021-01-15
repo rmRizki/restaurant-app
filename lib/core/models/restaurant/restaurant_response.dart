0b6f95e0bad658af961bf7288539e7e230daff85
Rizki M <rzrizkimaulana@gmail.com>
2021-01-16 05:39:15 +0700

    add models

---
+++ lib/core/models/restaurant/restaurant_response.dart
@@ -0,0 +1,67 @@
+import 'package:restaurant_app/core/models/menu/menus.dart';
+
+class RestaurantResponse {
+  List<Restaurants> restaurants;
+
+  RestaurantResponse({this.restaurants});
+
+  RestaurantResponse.fromJson(Map<String, dynamic> json) {
+    if (json['restaurants'] != null) {
+      restaurants = new List<Restaurants>();
+      json['restaurants'].forEach((v) {
+        restaurants.add(new Restaurants.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.restaurants != null) {
+      data['restaurants'] = this.restaurants.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Restaurants {
+  String id;
+  String name;
+  String description;
+  String pictureId;
+  String city;
+  num rating;
+  Menus menus;
+
+  Restaurants(
+      {this.id,
+      this.name,
+      this.description,
+      this.pictureId,
+      this.city,
+      this.rating,
+      this.menus});
+
+  Restaurants.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    description = json['description'];
+    pictureId = json['pictureId'];
+    city = json['city'];
+    rating = (json['rating']);
+    menus = json['menus'] != null ? new Menus.fromJson(json['menus']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['description'] = this.description;
+    data['pictureId'] = this.pictureId;
+    data['city'] = this.city;
+    data['rating'] = this.rating;
+    if (this.menus != null) {
+      data['menus'] = this.menus.toJson();
+    }
+    return data;
+  }
+}
