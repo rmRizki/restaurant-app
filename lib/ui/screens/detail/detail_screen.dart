57f2fd64e6840394804d31d16b558e5c4f355546
Rizki M <rzrizkimaulana@gmail.com>
2021-01-19 21:20:34 +0700

    add DetailScreen constructor

--- lib/ui/screens/detail/detail_screen.dart
+++ lib/ui/screens/detail/detail_screen.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_app/core/models/restaurant/restaurant_response.dart';
 
 class DetailScreen extends StatelessWidget {
+  static const routeName = 'detail';
+
+  final Restaurants restaurants;
+
+  DetailScreen({this.restaurants});
+
   @override
   Widget build(BuildContext context) {
     return Container();
