34f8583327dd12dfec3e9fda81b705843a152613
Rizki M <rzrizkimaulana@gmail.com>
2021-01-19 21:18:14 +0700

    add strings.dart

---
+++ lib/utils/sources/strings.dart
@@ -0,0 +1,15 @@
+class GlobalString {
+  static const title = 'Resto.';
+  static const search = 'Cari...';
+  static const page_not_found = 'Halaman Tidak Ditemukan';
+}
+
+class MainString {
+  static const detail = 'Detail';
+}
+
+class DetailString {
+  static const description = 'Deskripsi';
+  static const food = 'Makanan';
+  static const drink = 'Minuman';
+}
