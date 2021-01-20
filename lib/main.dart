8b342437a959f21e8de49e1e0ff0964ee40db3c9
Rizki M <rzrizkimaulana@gmail.com>
2021-01-20 17:27:02 +0700

    fix main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:restaurant_app/app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
     runApp(App());
