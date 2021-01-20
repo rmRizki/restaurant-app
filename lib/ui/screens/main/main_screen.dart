b01cc82d0451b746e974e08ef102c1d5b0c01d5d
Rizki M <rzrizkimaulana@gmail.com>
2021-01-20 17:27:17 +0700

    update main_screen.dart

--- lib/ui/screens/main/main_screen.dart
+++ lib/ui/screens/main/main_screen.dart
@@ -21,11 +21,58 @@ class _MainScreenState extends State<MainScreen> {
   _getRestaurantList() async {
     final data = await readJson(context, 'assets/local_restaurant.json');
     RestaurantResponse rs = RestaurantResponse.fromJson(data);
-    _restaurantList = rs.restaurants;
+    setState(() {
+      _restaurantList = rs.restaurants;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            leading: IconButton(
+                icon: Icon(Icons.filter_1),
+                onPressed: () {
+                  // Do something
+                }),
+            expandedHeight: 220.0,
+            floating: true,
+            pinned: true,
+            snap: true,
+            elevation: 50,
+            backgroundColor: Colors.pink,
+            flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                title: Text('Title',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    )),
+                background: Image.network(
+                  'https://images.pexels.com/photos/443356/pexels-photo-443356.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                  fit: BoxFit.cover,
+                )),
+          ),
+          new SliverList(
+              delegate:
+                  new SliverChildListDelegate(_buildList(_restaurantList))),
+        ],
+      ),
+    );
+  }
+
+  List _buildList(List<Restaurants> restaurants) {
+    List<Widget> listItems = [];
+
+    for (int i = 0; i < restaurants.length; i++) {
+      listItems.add(Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Text('Item ${restaurants[i].name}',
+              style: TextStyle(fontSize: 25.0))));
+    }
+
+    return listItems;
   }
 }
