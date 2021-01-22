dc07e5b5a1627937dd5d96a3cfcf6c5e67fa437c
Rizki M <rzrizkimaulana@gmail.com>
2021-01-22 20:00:14 +0700

    update customer_reviews.dart

--- lib/core/models/reviews/customer_reviews.dart
+++ lib/core/models/reviews/customer_reviews.dart
@@ -1,3 +1,33 @@
+class CustomerReviewsResponse {
+  bool error;
+  String message;
+  List<CustomerReviews> customerReviews;
+
+  CustomerReviewsResponse({this.error, this.message, this.customerReviews});
+
+  CustomerReviewsResponse.fromJson(Map<String, dynamic> json) {
+    error = json['error'];
+    message = json['message'];
+    if (json['customerReviews'] != null) {
+      customerReviews = [];
+      json['customerReviews'].forEach((v) {
+        customerReviews.add(CustomerReviews.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['error'] = this.error;
+    data['message'] = this.message;
+    if (this.customerReviews != null) {
+      data['customerReviews'] =
+          this.customerReviews.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
 class CustomerReviews {
   String name;
   String review;
