780b7272180de07aa8b39a13ceb0c7f53fbe148e
Rizki M <rzrizkimaulana@gmail.com>
2021-01-24 00:22:57 +0700

    implement search screen

--- lib/ui/screens/search/search_screen.dart
+++ lib/ui/screens/search/search_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:restaurant_app/ui/shared/component/custom_back_button.dart';
-import 'package:restaurant_app/ui/shared/component/scroll_floating_action_button.dart';
-import 'package:restaurant_app/ui/shared/component/search_field.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:restaurant_app/core/blocs/restaurant/list/restaurant_list_bloc.dart';
+import 'package:restaurant_app/core/models/models.dart';
+import 'package:restaurant_app/ui/screens/screens.dart';
+import 'package:restaurant_app/ui/shared/component/components.dart';
 import 'package:restaurant_app/ui/shared/separator/separator.dart';
 import 'package:restaurant_app/utils/sources/strings.dart';
 import 'package:restaurant_app/utils/styles/colors.dart';
@@ -17,11 +19,13 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   ScrollController _scrollController;
   TextEditingController _textEditingController;
+  RestaurantListBloc _restaurantListBloc;
 
   @override
   void initState() {
     _scrollController = ScrollController();
     _textEditingController = TextEditingController();
+    _restaurantListBloc = RestaurantListBloc();
     super.initState();
   }
 
@@ -32,34 +36,49 @@ class _SearchScreenState extends State<SearchScreen> {
     super.dispose();
   }
 
+  _onSearch(String query) {
+    _restaurantListBloc.add(RestaurantListSearched(query: query));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: ScrollFloatingActionButton(
         scrollController: _scrollController,
       ),
-      body: ListView(
-        controller: _scrollController,
-        physics: BouncingScrollPhysics(),
-        children: <Widget>[
-          CustomBackButton(),
-          _buildTitle(),
-          _buildSearchField(),
-          _buildSeparator(),
-          SizedBox(height: 16.0),
-        ],
+      body: BlocProvider(
+        create: (context) => _restaurantListBloc,
+        child: SingleChildScrollView(
+          controller: _scrollController,
+          physics: BouncingScrollPhysics(),
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CustomBackButton(),
+                _buildTitle(),
+                _buildSearchField(),
+                _buildSeparator(),
+                _buildContent(),
+                SizedBox(height: 16.0),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
 
   Widget _buildTitle() {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-      child: Text(
-        GlobalString.search,
-        style:
-            headingText.copyWith(fontWeight: FontWeight.normal, color: grey_80),
-        textAlign: TextAlign.center,
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+        child: Text(
+          GlobalString.search,
+          style: headingText.copyWith(
+              fontWeight: FontWeight.normal, color: grey_80),
+        ),
       ),
     );
   }
@@ -69,12 +88,119 @@ class _SearchScreenState extends State<SearchScreen> {
       padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       child: SearchField(
         controller: _textEditingController,
-        onSubmitted: (query) {
-          print('output search: $query');
-        },
+        onSubmitted: (query) => _onSearch(query),
         hintText: SearchString.restaurantName,
       ),
     );
+  }
+
+  Widget _buildContent() {
+    return BlocBuilder<RestaurantListBloc, RestaurantListState>(
+      builder: (context, state) {
+        if (state is RestaurantListInitial) {
+          _onSearch('');
+        }
+        if (state is RestaurantListLoadInProgress) {
+          return _buildLoading();
+        }
+        if (state is RestaurantListLoadFailure) {
+          return _buildError('${state.err}');
+        }
+        if (state is RestaurantListLoadSuccess) {
+          final restaurantList = state.restaurantList;
+          return _buildList(restaurantList);
+        }
+        return Container();
+      },
+    );
+  }
+
+  Widget _buildList(RestaurantList restaurantList) {
+    return restaurantList.founded == 0
+        ? _buildEmpty()
+        : ListView.builder(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              return _buildListItem(restaurantList, index);
+            },
+            itemCount: restaurantList.founded,
+            primary: false,
+          );
+  }
+
+  Widget _buildListItem(RestaurantList restaurantList, int index) {
+    Restaurants restaurant = restaurantList.restaurants[index];
+    return Container(
+      margin: EdgeInsets.only(right: 16.0, left: 16.0, top: 16.0),
+      child: RestaurantCard(
+        restaurant: restaurant,
+        onPressed: () => Navigator.pushNamed(
+          context,
+          DetailScreen.routeName,
+          arguments: restaurant,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoading() {
+    return _buildHeightContainer(
+        child: Center(child: CircularProgressIndicator()));
+  }
+
+  Widget _buildEmpty() {
+    return _buildHeightContainer(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Center(
+          child: Text(
+            SearchString.resultEmpty,
+            textAlign: TextAlign.center,
+            style: paragraphMedium.copyWith(color: grey_80),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildError(String err) {
+    return _buildHeightContainer(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              GlobalString.failed_request,
+              textAlign: TextAlign.center,
+              style: paragraphMedium.copyWith(color: grey_80),
+            ),
+            SizedBox(height: 4.0),
+            Text(
+              err,
+              textAlign: TextAlign.center,
+              style: smallCaption.copyWith(color: primary_100),
+            ),
+            SizedBox(height: 8.0),
+            MaterialButton(
+              onPressed: () => _onSearch(_textEditingController.text),
+              color: orange,
+              child: Text(GlobalString.reload,
+                  style: buttonLabel.copyWith(color: white)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeightContainer({Widget child}) {
+    return Container(
+        child: child,
+        height: MediaQuery.of(context).size.height / 2,
+        width: double.infinity);
   }
 
   Widget _buildSeparator() {
