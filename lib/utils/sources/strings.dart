716a9bd040ee9d0963fe3e3e472188bb3ca0124e
Rizki M <rzrizkimaulana@gmail.com>
2021-01-24 22:59:21 +0700

    update strings values

--- lib/utils/sources/strings.dart
+++ lib/utils/sources/strings.dart
@@ -4,6 +4,7 @@ class GlobalString {
   static const back = 'Kembali';
   static const page_not_found = 'Halaman Tidak Ditemukan';
   static const failed_request = 'Gagal Memuat Data...';
+  static const requesting = 'Memuat Data...';
   static const reload = 'Muat Ulang';
 }
 
@@ -21,6 +22,7 @@ class DetailString {
   static const food = 'Makanan';
   static const drink = 'Minuman';
   static const rating = 'Rating';
-  static const city = 'Alamat';
-  static const totalMenu = 'Total Menu';
+  static const city = 'Kota';
+  static const address = 'Alamat';
+  static const categories = 'Kategori';
 }
