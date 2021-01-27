06d5c0d874a45e880b7b1a750c0c23ab5a5fd9df
Rizki M <rzrizkimaulana@gmail.com>
2021-01-27 19:47:39 +0700

    simplify bloc test

--- test/blocs/restaurant_bloc_test.dart
+++ test/blocs/restaurant_bloc_test.dart
@@ -12,92 +12,41 @@ main() {
   group('RestaurantBloc', () {
     RestaurantBloc restaurantBloc;
 
-    setUp(() => restaurantBloc = MockRestaurantBloc());
+    setUp(() {
+      restaurantBloc = MockRestaurantBloc();
+      whenListen(
+          restaurantBloc,
+          Stream.fromIterable([
+            RestaurantInitial(),
+            RestaurantLoadInProgress(),
+            RestaurantListLoadSuccess(restaurantList: DummyRestaurant.list),
+            RestaurantDetailLoadSuccess(
+                restaurantDetail: DummyRestaurant.detail),
+            RestaurantLoadFailure(err: 'dummyError'),
+          ]));
+    });
 
     tearDown(() => restaurantBloc.close());
 
-    test('bloc initial state', () async {
-      whenListen(restaurantBloc, Stream.value(RestaurantInitial()));
-      await expectLater(restaurantBloc, emits(RestaurantInitial()));
-      expect(restaurantBloc.state, RestaurantInitial());
-    });
-
     blocTest(
-      'bloc request list',
-      build: () {
-        whenListen(
-            restaurantBloc,
-            Stream.fromIterable([
-              RestaurantInitial(),
-              RestaurantLoadInProgress(),
-              RestaurantListLoadSuccess(restaurantList: DummyRestaurant.list),
-            ]));
-        return restaurantBloc;
-      },
+      'test bloc, event and state',
+      build: () => restaurantBloc,
       act: (RestaurantBloc bloc) {
         bloc.add(RestaurantListRequested());
-      },
-      expect: [
-        RestaurantInitial(),
-        RestaurantLoadInProgress(),
-        RestaurantListLoadSuccess(restaurantList: DummyRestaurant.list),
-      ],
-      verify: (RestaurantBloc bloc) {
-        verify(bloc.add(RestaurantListRequested())).called(1);
-        verify(bloc.close()).called(1);
-      },
-    );
-
-    blocTest(
-      'bloc search list',
-      build: () {
-        whenListen(
-            restaurantBloc,
-            Stream.fromIterable([
-              RestaurantInitial(),
-              RestaurantLoadInProgress(),
-              RestaurantListLoadSuccess(restaurantList: DummyRestaurant.list),
-            ]));
-        return restaurantBloc;
-      },
-      act: (RestaurantBloc bloc) {
         bloc.add(RestaurantListSearched(query: 'food'));
-      },
-      expect: [
-        RestaurantInitial(),
-        RestaurantLoadInProgress(),
-        RestaurantListLoadSuccess(restaurantList: DummyRestaurant.list),
-      ],
-      verify: (RestaurantBloc bloc) {
-        verify(bloc.add(RestaurantListSearched(query: 'food'))).called(1);
-        verify(bloc.close()).called(1);
-      },
-    );
-
-    blocTest(
-      'bloc request detail',
-      build: () {
-        whenListen(
-            restaurantBloc,
-            Stream.fromIterable([
-              RestaurantInitial(),
-              RestaurantLoadInProgress(),
-              RestaurantDetailLoadSuccess(
-                  restaurantDetail: DummyRestaurant.detail),
-            ]));
-        return restaurantBloc;
-      },
-      act: (RestaurantBloc bloc) {
         bloc.add(RestaurantDetailRequested(id: 'rqdv5juczeskfw1e867'));
       },
       expect: [
         RestaurantInitial(),
         RestaurantLoadInProgress(),
+        RestaurantListLoadSuccess(restaurantList: DummyRestaurant.list),
         RestaurantDetailLoadSuccess(restaurantDetail: DummyRestaurant.detail),
+        RestaurantLoadFailure(err: 'dummyError'),
       ],
       verify: (RestaurantBloc bloc) {
-        verify(bloc.add(RestaurantDetailRequested(id: 'rqdv5juczeskfw1e867')))
-            .called(1);
+        verify(bloc.add(RestaurantListRequested())).called(1);
+        verify(bloc.add(RestaurantListSearched(query: 'food'))).called(1);
+        verify(bloc.add(RestaurantDetailRequested(id: 'rqdv5juczeskfw1e867')));
         verify(bloc.close()).called(1);
       },
     );
