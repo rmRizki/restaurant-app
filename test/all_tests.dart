018d90a29928b7851618de0cfdeb1473de8271c8
Rizki M <rzrizkimaulana@gmail.com>
2021-01-27 12:05:25 +0700

    init test files

---
+++ test/all_tests.dart
@@ -0,0 +1,9 @@
+import './blocs/restaurant_bloc_test.dart' as restaurant_bloc;
+import './models/restaurant_detail_test.dart' as restaurant_detail;
+import './models/restaurant_list_test.dart' as restaurant_list;
+
+main() {
+  restaurant_bloc.main();
+  restaurant_detail.main();
+  restaurant_list.main();
+}
