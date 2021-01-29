596876207067e621afb2aeb0b8a77ddf8bb32fc5
Rizki M <rzrizkimaulana@gmail.com>
2021-01-30 00:14:37 +0700

    add fav bloc  test

--- test/all_tests.dart
+++ test/all_tests.dart
@@ -1,4 +1,5 @@
 import './blocs/detail_bloc_test.dart' as detail_bloc;
+import './blocs/favorite_bloc_test.dart' as favorite_bloc;
 import './blocs/main_bloc_test.dart' as main_bloc;
 import './blocs/search_bloc_test.dart' as search_bloc;
 import './models/restaurant_detail_test.dart' as restaurant_detail;
@@ -9,6 +10,7 @@ import './repositories/restaurant_repository_test.dart'
 
 main() {
   detail_bloc.main();
+  favorite_bloc.main();
   main_bloc.main();
   search_bloc.main();
   restaurant_detail.main();
---
+++ test/blocs/favorite_bloc_test.dart
@@ -0,0 +1,51 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:restaurant_app/core/blocs/blocs.dart';
+
+import '../dummy/values.dart';
+
+class MockFavoriteBloc extends MockBloc<FavoriteState> implements FavoriteBloc {
+}
+
+main() {
+  group('FavoriteBloc', () {
+    FavoriteBloc favoriteBloc;
+
+    setUp(() {
+      favoriteBloc = MockFavoriteBloc();
+      whenListen(
+          favoriteBloc,
+          Stream.fromIterable([
+            FavoriteInitial(),
+            FavoriteLoadInProgress(),
+            FavoriteLoadSuccess(restaurantList: DummyRestaurant.list),
+          ]));
+    });
+
+    tearDown(() => favoriteBloc.close());
+
+    blocTest(
+      'test bloc, event and state',
+      build: () => favoriteBloc,
+      act: (FavoriteBloc bloc) => bloc
+        ..add(FavoriteBoxStarted())
+        ..add(
+            FavoriteAdded(restaurant: DummyRestaurant.list.restaurants?.first))
+        ..add(FavoriteRemoved(id: DummyRestaurant.id)),
+      expect: [
+        FavoriteInitial(),
+        FavoriteLoadInProgress(),
+        FavoriteLoadSuccess(restaurantList: DummyRestaurant.list),
+      ],
+      verify: (FavoriteBloc bloc) {
+        verify(bloc.add(FavoriteBoxStarted())).called(1);
+        verify(bloc.add(FavoriteAdded(
+                restaurant: DummyRestaurant.list.restaurants?.first)))
+            .called(1);
+        verify(bloc.add(FavoriteRemoved(id: DummyRestaurant.id))).called(1);
+        verify(bloc.close()).called(1);
+      },
+    );
+  });
+}
