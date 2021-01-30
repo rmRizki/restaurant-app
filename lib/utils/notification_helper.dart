80fbe314cf6025083796d400896d606350128551
Rizki M <rzrizkimaulana@gmail.com>
2021-01-30 15:42:55 +0700

    add notification helper

---
+++ lib/utils/notification_helper.dart
@@ -0,0 +1,80 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:restaurant_app/core/models/models.dart';
+import 'package:restaurant_app/utils/navigation.dart';
+import 'package:rxdart/rxdart.dart';
+
+final selectNotificationSubject = BehaviorSubject<String>();
+
+class NotificationHelper {
+  static NotificationHelper _instance;
+
+  NotificationHelper._internal() {
+    _instance = this;
+  }
+
+  factory NotificationHelper() => _instance ?? NotificationHelper._internal();
+
+  Future<void> initNotifications(
+      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
+    var initializationSettingsAndroid =
+        AndroidInitializationSettings('app_icon');
+
+    var initializationSettingsIOS = IOSInitializationSettings(
+      requestAlertPermission: false,
+      requestBadgePermission: false,
+      requestSoundPermission: false,
+    );
+
+    var initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: (String payload) async {
+      if (payload != null) {
+        print('notification payload: ' + payload);
+      }
+      selectNotificationSubject.add(payload);
+    });
+  }
+
+  Future<void> showNotification(
+      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin,
+      RestaurantList restaurantList) async {
+    var _channelId = "1";
+    var _channelName = "channel_01";
+    var _channelDescription = "resto channel";
+
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        _channelId, _channelName, _channelDescription,
+        importance: Importance.max,
+        priority: Priority.high,
+        ticker: 'ticker',
+        styleInformation: DefaultStyleInformation(true, true));
+
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        android: androidPlatformChannelSpecifics,
+        iOS: iOSPlatformChannelSpecifics);
+
+    var titleNotification = "<b>Rekomendasi Resto.</b>";
+    var randomIndex = Random().nextInt(restaurantList.restaurants.length - 1);
+    Restaurants selectedRestaurant = restaurantList.restaurants[randomIndex];
+    var titleRestaurant = selectedRestaurant.name;
+
+    await flutterLocalNotificationsPlugin.show(
+        0, titleNotification, titleRestaurant, platformChannelSpecifics,
+        payload: json.encode(selectedRestaurant.toJson()));
+  }
+
+  void configureSelectNotificationSubject(String route) {
+    selectNotificationSubject.stream.listen(
+      (String payload) async {
+        var data = Restaurants.fromJson(json.decode(payload));
+        Navigation.pushNamed(route, data);
+      },
+    );
+  }
+}
