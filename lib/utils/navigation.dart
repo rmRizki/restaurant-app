1d54c9dde6cb607f9db7173581035bde8b183058
Rizki M <rzrizkimaulana@gmail.com>
2021-01-30 15:37:16 +0700

    add navigation helper

---
+++ lib/utils/navigation.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+ 
+class Navigation {
+  static pushNamed(String routeName, Object arguments) {
+    navigatorKey.currentState.pushNamed(routeName, arguments: arguments);
+  }
+ 
+  static back() => navigatorKey.currentState.pop();
+}
